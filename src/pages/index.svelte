73af8cf8c683b35b74f8455bd7eea767a24f4014
Ben Sooter <sooterbw@gmail.com>
2021-02-03 22:05:24 -0600

    initial commit

---
+++ src/pages/index.svelte
@@ -0,0 +1,9 @@
+<script>
+    import RoutifyIntro from './example/_components/RoutifyIntro.svelte'
+    import { metatags } from '@roxi/routify'
+    metatags.title = 'Routify Tailwind Starter Template'
+    metatags.description = 'Description coming soon...'
+</script>
+
+<div class="text-3xl font-bold">Hello World!</div>
+<RoutifyIntro />
